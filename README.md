19cd5a10a61476d8e5d2a81bb4b458b68586b1f7
Yohan Belval <yohan.belval@gsoft.com>
2021-07-12 22:23:38 -0400

    Added helm diff plugin install

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# Helmfile installer for Azure Pipelines
+# Helmfile runner for Azure Pipelines
 
-This is an [Azure DevOps extension](https://marketplace.visualstudio.com/items?itemName=GSoft.HelmfileInstaller) that allows you to install the [helmfile cli tool](https://github.com/roboll/helmfile).
+This is an [Azure DevOps extension](https://marketplace.visualstudio.com/items?itemName=GSoft.HelmfileRunner) that allows you to run the [helmfile cli tool](https://github.com/roboll/helmfile).
 
 ## Installation
 
-Helmfile installer for Azure Pipelines can be installed from the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=GSoft.HelmfileInstaller).
+Helmfile runner for Azure Pipelines can be installed from the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=GSoft.HelmfileRunner).
+
+## Usage
+
+**Prerequesites:** To use this extension, you must have the [Helm](https://docs.microsoft.com/en-us/azure/devops/pipelines/tasks/tool/helm-installer?view=azure-devops) and [Helmfile](https://marketplace.visualstudio.com/items?itemName=GSoft.HelmfileInstaller) cli tools installed beforehand.
 
 ## Compiling
 
@@ -12,4 +16,4 @@ There are GitHub actions that take care of compiling, packaging and publishing t
 
 ## License
 
-Copyright © 2021, Groupe GSoft Inc. This code is licensed under the Apache License, Version 2.0. You may obtain a copy of this license at https://github.com/gsoft-inc/azure-pipelines-helmfile-installer/blob/main/LICENSE.
+Copyright © 2021, Groupe GSoft Inc. This code is licensed under the Apache License, Version 2.0. You may obtain a copy of this license at https://github.com/gsoft-inc/azure-pipelines-helmfile-runner/blob/main/LICENSE.
--- task/src/index.ts
+++ task/src/index.ts
@@ -1,92 +1,102 @@
-import * as path from "path";
-import * as fs from "fs";
-import * as tl from "azure-pipelines-task-lib/task";
-import { IExecSyncResult } from "azure-pipelines-task-lib/toolrunner";
-import { helmfileCommandBuilder } from "./helmfileCommandBuilder";
-import k8sCommand from "./k8sCommand";
-import * as utils from "./utils";
+import * as path from 'path';
+import * as fs from 'fs';
+import * as tl from 'azure-pipelines-task-lib/task';
+import { IExecSyncResult } from 'azure-pipelines-task-lib/toolrunner';
+import { helmfileCommandBuilder } from './helmfileCommandBuilder';
+import k8sCommand from './k8sCommand';
+import * as utils from './utils';
+import helmCommand from './helmCommand';
 
 const environmentVariableMaximumSize = 32766;
 
 function isKubConfigSetupRequired(_command: string): boolean {
-    var connectionType = tl.getInput("connectionType", true);
-    return connectionType !== "None";
+  var connectionType = tl.getInput('connectionType', true);
+  return connectionType !== 'None';
 }
 
 function isKubConfigLogoutRequired(command: string): boolean {
-    var connectionType = tl.getInput("connectionType", true);
-    return command !== "login" && connectionType !== "None";
+  var connectionType = tl.getInput('connectionType', true);
+  return command !== 'login' && connectionType !== 'None';
 }
 
 function getClusterType(): any {
-    var connectionType = tl.getInput("connectionType", true);
-    var endpoint = tl.getInput("azureSubscriptionEndpoint")
-    if (connectionType === "Azure Resource Manager" && endpoint) {
-        return require("./clusters/armkubernetescluster");
-    }
+  var connectionType = tl.getInput('connectionType', true);
+  var endpoint = tl.getInput('azureSubscriptionEndpoint');
+  if (connectionType === 'Azure Resource Manager' && endpoint) {
+    return require('./clusters/armkubernetescluster');
+  }
 
-    return require("./clusters/generickubernetescluster");
+  return require('./clusters/generickubernetescluster');
 }
 
 function getKubeConfigFilePath(): string {
-    var userdir = utils.getTaskTempDir();
-    return path.join(userdir, "config");
+  var userdir = utils.getTaskTempDir();
+  return path.join(userdir, 'config');
 }
 
 async function getKubeConfigFile(): Promise<string> {
-    return getClusterType().getKubeConfig().then((config) => {
-        var configFilePath = getKubeConfigFilePath();
-        tl.debug(`KubeConfigFilePath: ${configFilePath}`);
-        fs.writeFileSync(configFilePath, config);
-        fs.chmodSync(configFilePath, '600');
-        return configFilePath;
+  return getClusterType()
+    .getKubeConfig()
+    .then(config => {
+      var configFilePath = getKubeConfigFilePath();
+      tl.debug(`KubeConfigFilePath: ${configFilePath}`);
+      fs.writeFileSync(configFilePath, config);
+      fs.chmodSync(configFilePath, '600');
+      return configFilePath;
     });
 }
 
 async function run() {
-    var command = tl.getInput("command", true).toLowerCase();
-    var isKubConfigRequired = isKubConfigSetupRequired(command);
-    var k8s: k8sCommand;
-    if (isKubConfigRequired) {
-        var kubeconfigfilePath = command === "logout" ? tl.getVariable("KUBECONFIG") : await getKubeConfigFile();
-        k8s = new k8sCommand(kubeconfigfilePath);
-        k8s.login();
-    }
+  var command = tl.getInput('command', true).toLowerCase();
+  var isKubConfigRequired = isKubConfigSetupRequired(command);
+  var k8s: k8sCommand;
+  if (isKubConfigRequired) {
+    var kubeconfigfilePath = command === 'logout' ? tl.getVariable('KUBECONFIG') : await getKubeConfigFile();
+    k8s = new k8sCommand(kubeconfigfilePath);
+    k8s.login();
+  }
 
-    try {
-        switch (command) {
-            case "login":
-                k8s.setKubeConfigEnvVariable();
-                break;
-            case "logout":
-                k8s.unsetKubeConfigEnvVariable();
-                break;
-            default:
-                const failOnStderr = tl.getBoolInput("failOnStderr");
-                const execResult: IExecSyncResult = new helmfileCommandBuilder(command).build().execHelmfileCommand();
-                if (execResult.code != tl.TaskResult.Succeeded || !!execResult.error || (failOnStderr && !!execResult.stderr)) {
-                    tl.debug('execResult: ' + JSON.stringify(execResult));
-                    tl.setResult(tl.TaskResult.Failed, execResult.stderr);
-                }
-                if (execResult.stdout) {
-                    var commandOutputLength = execResult.stdout.length;
-                    if (commandOutputLength > environmentVariableMaximumSize) {
-                        tl.warning(`OutputVariableDataSizeExceeded: commandOutputLength: ${commandOutputLength}, environmentVariableMaximumSize: ${environmentVariableMaximumSize}`);
-                    } else {
-                        tl.setVariable("helmfileOutput", execResult.stdout);
-                    }
-                }
+  try {
+    const failOnStderr = tl.getBoolInput('failOnStderr');
+    switch (command) {
+      case 'diff':
+        const installUpdateHelmDiff = tl.getBoolInput('installUpdateHelmDiff');
+        if (installUpdateHelmDiff) {
+          const plugInstallResult = new helmCommand().execHelmPluginInstallCommand('diff', 'https://github.com/databus23/helm-diff');
+          if (
+            plugInstallResult.code != tl.TaskResult.Succeeded ||
+            !!plugInstallResult.error ||
+            (failOnStderr && !!plugInstallResult.stderr)
+          ) {
+            tl.debug('execResult: ' + JSON.stringify(plugInstallResult));
+            tl.setResult(tl.TaskResult.Failed, plugInstallResult.stderr);
+          }
         }
-
-    } catch (err) {
-        // not throw error so that we can logout from kubernetes
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    }
-    finally {
-        if (isKubConfigLogoutRequired(command)) {
-            k8s.logout();
+      default:
+        const execResult: IExecSyncResult = new helmfileCommandBuilder(command).build().execHelmfileCommand();
+        if (execResult.code != tl.TaskResult.Succeeded || !!execResult.error || (failOnStderr && !!execResult.stderr)) {
+          tl.debug('execResult: ' + JSON.stringify(execResult));
+          tl.setResult(tl.TaskResult.Failed, execResult.stderr);
+        }
+        if (execResult.stdout) {
+          var commandOutputLength = execResult.stdout.length;
+          if (commandOutputLength > environmentVariableMaximumSize) {
+            tl.warning(
+              `OutputVariableDataSizeExceeded: commandOutputLength: ${commandOutputLength}, environmentVariableMaximumSize: ${environmentVariableMaximumSize}`
+            );
+          } else {
+            tl.setVariable('helmfileOutput', execResult.stdout);
+          }
         }
     }
+  } catch (err) {
+    // not throw error so that we can logout from kubernetes
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  } finally {
+    if (isKubConfigLogoutRequired(command)) {
+      k8s.logout();
+    }
+  }
 }
 
 run();
